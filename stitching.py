2768bd63e9f151d8a40a5b9b69d31b3881b7fbbc
jonborrego96 <jon.borrego@estudiantat.upc.edu>
2025-05-28 11:04:24 +0200

    Update stitching.py

--- stitching.py
+++ stitching.py
@@ -7,7 +7,6 @@ def plot_image(img, figsize_in_inches=(5,5)):
     ax.imshow(cv.cvtColor(img, cv.COLOR_BGR2RGB))
     plt.show()
 
-#stitcher = Stitcher()
 stitcher = Stitcher(detector="sift", confidence_threshold=0.)
 panorama = stitcher.stitch(["imatge34.jpg", "imatge35.jpg"])
 plot_image(panorama, (20,20))
